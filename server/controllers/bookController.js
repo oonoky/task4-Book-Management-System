bc34ae238139e4282d620041e6e89499c2cedddf
Gulnaz Omirzakova <gulnaz.omirzakova@narxoz.kz>
2023-11-12 02:16:31 +0600

    task

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -7,50 +7,19 @@ const mongoose = require("mongoose");
  * Homepage
  */
 exports.homepage = async (req, res) => {
-  const messages = await req.flash('info');
-  const locals = {
-      title:'NodeJs',
-      description: 'Free NodeJs Book Management System'
-  }
-  let perPage = 12;
-  let page = req.query.page || 1;
-  try{
-    const books = await Book.aggregate([ { $sort: { createdAt: -1 } } ])
-    .skip(perPage * page - perPage)
-    .limit(perPage)
-    .exec(); 
-  // Count is deprecated. Use countDocuments({}) or estimatedDocumentCount() 
-  // const count = await Customer.count();
-    const count = await Book.countDocuments({});
-    res.render('index', {
-      locals,
-      books,
-      current: page,
-      pages: Math.ceil(count / perPage),
-      messages
-    });
-  }catch (error) {
-    console.log(error);
-  }
-  
-
-}// 
-// exports.homepage = async (req, res) => {
-//     const messages = await req.flash('info');
-//     const locals = {
-//         title:'NodeJs',
-//         description: 'Free NodeJs Book Management System'
-//     }
-
-//     try{
-//       const books = await Book.find({}).limit(22);
-//       res.render('index', { locals, messages, books });
-//     }catch (error) {
-//       console.log(error);
-//     }
+    const messages = await req.flash('info');
+    const locals = {
+        title:'NodeJs',
+        description: 'Free NodeJs Book Management System'
+    }
+    try{
+      const books = await Book.find({}).limit(22);
+      res.render('index', { locals, messages, books });
+    }catch (error) {
+      console.log(error);
+    }
     
-
-// }
+}
 
 /**
  * GET /
